28a7d1f467a9f3c5cc658703f97ecf65ae8c5559
Scot MacDonald <scotmacdonald@hotmail.com>
2025-11-11 14:55:56 +0100

    map style

--- src/blocks/KontaktAnfahrt/Map.tsx
+++ src/blocks/KontaktAnfahrt/Map.tsx
@@ -45,8 +45,8 @@ export default function Map({
       style={{ height: '400px', width: '100%' }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution="&copy; <a href='https://carto.com/'>CARTO</a>"
       />
 
       {/* Main marker */}
